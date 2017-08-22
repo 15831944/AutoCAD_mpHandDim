e014e61889efcc246ecf41aa449875c9d474a7d3
cjmodis <cjmodis@gmail.com>
2017-08-22 22:03:28 +0300

    Добавьте файлы проекта.

---
+++ mpHandDim_2017/Interface.cs
@@ -0,0 +1,18 @@
+﻿using mpPInterface;
+
+namespace mpHandDim
+{
+    public class Interface : IPluginInterface
+    {
+        public string Name => "mpHandDim";
+        public string AvailCad => "2017";
+        public string LName => "Ручные размеры";
+        public string Description => "Выделение цветом, восстановление или удаление размеров с переопределенным значением";
+        public string Author => "Пекшев Александр aka Modis";
+        public string Price => "0";
+    }
+    public class VersionData
+    {
+        public const string FuncVersion = "2017";
+    }
+}
